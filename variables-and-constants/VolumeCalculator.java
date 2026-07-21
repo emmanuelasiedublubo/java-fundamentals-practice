f0ba5f76579ea25d47b02da6c7dd5e5bda49e06b
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-07-21 14:37:18 +0000

    Add VolumeCalculator Java program

---
+++ variables-and-constants/VolumeCalculator.java
@@ -0,0 +1,18 @@
+public class VolumeCalculator {
+
+    public static void main(String[] args) {
+
+        int length = 10;
+        int width = 12;
+        int height = 15;
+
+        final int volume = length * width * height;
+
+        System.out.println("Length: " + length);
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+        System.out.println("Volume: " + volume);
+
+    }
+
+}
