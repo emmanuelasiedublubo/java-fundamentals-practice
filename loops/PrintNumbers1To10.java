13020fc7384808ecb3c90e713eca237b45dc25b6
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-08-07 09:52:17 +0000

    Add PrintNumbers1To10 program

---
+++ loops/PrintNumbers1To10.java
@@ -0,0 +1,19 @@
+package loops;
+
+/*
+ * Program: PrintNumbers1To10
+ * Module: Loops
+ * Purpose: Prints the numbers 1 to 10 using a for loop.
+ */
+
+public class PrintNumbers1To10 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
