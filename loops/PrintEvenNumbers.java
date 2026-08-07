0fd73f47310fa5606efc6cce2f51d3f9ecca9c47
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-08-07 09:57:30 +0000

    Add PrintEvenNumbers program

---
+++ loops/PrintEvenNumbers.java
@@ -0,0 +1,19 @@
+package loops;
+
+/*
+ * Program: PrintEvenNumbers
+ * Module: Loops
+ * Purpose: Prints all even numbers from 2 to 20 using a for loop.
+ */
+
+public class PrintEvenNumbers {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+
+    }
+
+}
