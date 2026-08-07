0ffd45d0ffb53cfceab6b9f2719ed660ccb97462
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-08-07 14:57:43 +0000

    Add SimpleMethod program

---
+++ methods/SimpleMethod.java
@@ -0,0 +1,21 @@
+package methods;
+
+/*
+ * Program: SimpleMethod
+ * Module: Methods
+ * Purpose: Demonstrates how to create and call a simple Java method.
+ */
+
+public class SimpleMethod {
+
+    public static void displayMessage() {
+        System.out.println("Welcome to Java methods!");
+    }
+
+    public static void main(String[] args) {
+
+        displayMessage();
+
+    }
+
+}
