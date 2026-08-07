8031b656542ab603246709bda711b97a820c13bf
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-08-07 08:59:12 +0000

    Add LeapYearChecker program

---
+++ conditionals/LeapYearChecker.java
@@ -0,0 +1,23 @@
+package conditionals;
+
+/*
+ * Program: LeapYearChecker
+ * Module: Conditionals
+ * Purpose: Determines whether a given year is a leap year.
+ */
+
+public class LeapYearChecker {
+
+    public static void main(String[] args) {
+
+        int year = 2024;
+
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is Not a Leap Year.");
+        }
+
+    }
+
+}
