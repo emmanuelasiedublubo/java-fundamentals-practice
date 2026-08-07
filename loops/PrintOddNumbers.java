a42b31b41d901e966f50d11a5f6e53102f6e1deb
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-08-07 09:59:35 +0000

    Add PrintOddNumbers program

---
+++ loops/PrintOddNumbers.java
@@ -0,0 +1,19 @@
+package loops;
+
+/*
+ * Program: PrintOddNumbers
+ * Module: Loops
+ * Purpose: Prints all odd numbers from 1 to 19 using a for loop.
+ */
+
+public class PrintOddNumbers {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+
+    }
+
+}
