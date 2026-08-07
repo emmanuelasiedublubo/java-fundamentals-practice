0d2f7bb584244787ee84fc6c1d188fd04e33b28a
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-08-07 14:25:57 +0000

    Add PrintArray program

---
+++ arrays/PrintArray.java
@@ -0,0 +1,21 @@
+package arrays;
+
+/*
+ * Program: PrintArray
+ * Module: Arrays
+ * Purpose: Stores and prints elements of an integer array.
+ */
+
+public class PrintArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+
+}
