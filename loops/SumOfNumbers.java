4037f77f30e93d8b1887c1f4d5f9bf4ecaeb8695
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-08-07 10:01:07 +0000

    Add SumOfNumbers program

---
+++ loops/SumOfNumbers.java
@@ -0,0 +1,23 @@
+package loops;
+
+/*
+ * Program: SumOfNumbers
+ * Module: Loops
+ * Purpose: Calculates the sum of the numbers from 1 to 10 using a for loop.
+ */
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+
+    }
+
+}
