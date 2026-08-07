252fa684de962a5e0b1de056a5bc018f59eecd3b
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-08-07 15:24:11 +0000

    Add DayOfWeekSwitch program

---
+++ switchstatements/DayOfWeekSwitch.java
@@ -0,0 +1,53 @@
+package switchstatements;
+
+/*
+ * Program: DayOfWeekSwitch
+ * Module: Switch Statements
+ * Purpose: Uses a switch statement to display a day of the week
+ * based on a numeric value.
+ */
+
+public class DayOfWeekSwitch {
+
+    public static void main(String[] args) {
+
+        int day = 3;
+
+        switch (day) {
+
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+
+        }
+
+    }
+
+}
