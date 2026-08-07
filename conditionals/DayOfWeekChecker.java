9b4e6312d6d7170b3a404fc8d7da0b7874286969
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-08-07 09:04:08 +0000

    Add DayOfWeekChecker program

---
+++ conditionals/DayOfWeekChecker.java
@@ -0,0 +1,35 @@
+package conditionals;
+
+/*
+ * Program: DayOfWeekChecker
+ * Module: Conditionals
+ * Purpose: Displays the day of the week based on a number.
+ */
+
+public class DayOfWeekChecker {
+
+    public static void main(String[] args) {
+
+        int day = 3;
+
+        if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6) {
+            System.out.println("Saturday");
+        } else if (day == 7) {
+            System.out.println("Sunday");
+        } else {
+            System.out.println("Invalid day.");
+        }
+
+    }
+
+}
