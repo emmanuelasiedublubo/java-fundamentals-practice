0a41269d523f2ce92b46aad66b48a808391a1b8b
Emmanuel Kojo Asiedu-Blubo <emmanuelblubo@gmail.com>
2026-08-07 14:34:42 +0000

    Add AverageArray program

---
+++ arrays/AverageArray.java
@@ -0,0 +1,26 @@
+package arrays;
+
+/*
+ * Program: AverageArray
+ * Module: Arrays
+ * Purpose: Calculates the average of all elements in an integer array.
+ */
+
+public class AverageArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("Average: " + average);
+
+    }
+
+}
